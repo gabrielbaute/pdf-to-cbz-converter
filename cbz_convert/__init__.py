738b7892a7e13a3867298c0e7f1bb9e2a64bebc8
gabrielbaute <gabrielbaute@gmail.com>
2025-04-26 23:28:23 -0400

    Version 0.2.0

--- cbz_convert/__init__.py
+++ cbz_convert/__init__.py
@@ -1,5 +1,5 @@
 from cbz_convert.core.converter import PDFtoCBZConverter
 from cbz_convert.cli.main import main
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["PDFtoCBZConverter", "main"]
